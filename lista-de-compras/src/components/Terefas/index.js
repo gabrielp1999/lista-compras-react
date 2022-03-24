8d3ffc2b52993cf2bcc613495588ea9515c571c8
Gabriel <gabriel.gsm2017@gmail.com>
2022-03-24 17:03:24 -0300

    finalizando o projeto em react

--- lista-de-compras/src/components/Terefas/index.js
+++ lista-de-compras/src/components/Terefas/index.js
@@ -3,8 +3,26 @@ import React, { useState } from "react";
 // Criar uma função que checa se o checkbox esta marcado
 
 function Tarefas() {
-  const [lista, setLista] = useState([]);
+  const [listaTarefas, setListaTarefas] = useState([]);
   const [produto, setProduto] = useState("");
+
+  function marcarCheckbox(index) {
+    const novaLista = listaTarefas.map((itemProduto, indexProduto) => {
+
+      if(indexProduto === index){
+        return {
+          ...itemProduto,
+          marcado: !itemProduto.marcado
+        }
+      }
+      
+      return itemProduto;
+    })
+    setListaTarefas(novaLista)
+  }
+
+  const filtrados = listaTarefas.filter(value => value.marcado);
+  const result = filtrados.length;
   
   return(
     <div className="Tarefa">
@@ -14,17 +32,30 @@ function Tarefas() {
         onChange={e => setProduto(e.target.value)}
         value={produto}
       />
-      <button onClick={() => setLista([...lista, produto])}>Listar Compra</button>
-      <div>{lista.map(item => (
-        <div className="item"><input
-         className="caixa"
+      <button 
+        onClick={() =>{
+           if(produto.length < 2){
+           return alert('digite o item que deseja comprar');
+          }
+         setListaTarefas([...listaTarefas, {
+          produto:produto, marcado:false 
+        }])} } >
+        Listar Compra
+      </button>
+
+      <h5> <span>{result}</span> iten(s) no carrinho</h5>
+
+      <div>{listaTarefas.map((item, index) => (
+        <div className="item">
+          <input
+          className="caixa"
           type="checkbox"
-          
-          />{item}</div>
+          defaultChecked={item.marcado}
+          onChange={() => marcarCheckbox(index)}
+          />{item.produto}</div>
         ))}
       </div>
     </div>
   )
 }
-
 export default Tarefas;
